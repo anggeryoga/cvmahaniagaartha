185a922928fdf27cd91bff3b0fb6d40ce084c7d8
anggeryoga <139601862+anggeryoga@users.noreply.github.com>
2025-08-21 09:44:24 +0700

    Publish application

--- src/utils/adminUtils.ts
+++ src/utils/adminUtils.ts
@@ -4,24 +4,23 @@ const adminDetails = [
   { number: '6282124952606', name: 'Admin Livia' }
 ];
 
-// Product URL mapping
-const productUrls: { [key: string]: string } = {
-  'Chick Ichick': 'https://paketusaha.web.id/p/chick-ichik',
-  'Chickuruyuk': 'https://paketusaha.web.id/p/chikuruyuk',
-  'Esteh Ibukota': 'https://paketusaha.web.id/p/esteh-ibukota',
-  'Jafa Boga (Bubuk)': 'https://paketusaha.web.id/p/jafa-boga-bubuk',
-  'K-Mie': 'https://paketusaha.web.id/p/k-mie',
-  'Kentang Gantenk': 'https://paketusaha.web.id/p/kentang-gantenk',
-  'Kopi Ibukota': 'https://paketusaha.web.id/p/kopi-ibukota',
-  'Mentoast': 'https://paketusaha.web.id/p/mentoast',
-  'Merlumer': 'https://paketusaha.web.id/p/merlumer',
-  'Mybestea': 'https://paketusaha.web.id/p/mybestea',
-  'Nice Coffee': 'https://paketusaha.web.id/p/nice-coffee',
-  'Raja Steak': 'https://paketusaha.web.id/p/raja-steak',
-  'Seblak Express': 'https://paketusaha.web.id/p/seblak-express',
-  'Tahu Nyonyor': 'https://paketusaha.web.id/p/tahu-nyonyor',
-  'You Need Mie': 'https://paketusaha.web.id/p/you-need-mie'
+const productUrls = {
+  "Chick Ichik": "https://paketusaha.web.id/p/chick-ichik",
+  "Chikuruyuk": "https://paketusaha.web.id/p/chikuruyuk",
+  "Esteh Ibukota": "https://paketusaha.web.id/p/esteh-ibukota",
+  "K-Mie": "https://paketusaha.web.id/p/k-mie",
+  "Kentang Gantenk": "https://paketusaha.web.id/p/kentang-gantenk",
+  "Kopi Ibukota": "https://paketusaha.web.id/p/kopi-ibukota",
+  "Mentoast": "https://paketusaha.web.id/p/mentoast",
+  "Merlumer": "https://paketusaha.web.id/p/merlumer",
+  "Mybestea": "https://paketusaha.web.id/p/mybestea",
+  "Nice Coffee": "https://paketusaha.web.id/p/nice-coffee",
+  "Raja Steak": "https://paketusaha.web.id/p/raja-steak",
+  "Seblak Express": "https://paketusaha.web.id/p/seblak-express",
+  "Tahu Nyonyor": "https://paketusaha.web.id/p/tahu-nyonyor",
+  "You Need Mie": "https://paketusaha.web.id/p/you-need-mie"
 };
+
 
 export const getRandomAdmin = () => {
   const randomIndex = Math.floor(Math.random() * adminDetails.length);
