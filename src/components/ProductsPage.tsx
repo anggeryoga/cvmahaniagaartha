4486e20019b4e3c4dc99022b437ff2b6a7db8ad6
anggeryoga <139601862+anggeryoga@users.noreply.github.com>
2025-10-14 08:37:44 +0700

    @jafabogaartha

--- src/components/ProductsPage.tsx
+++ src/components/ProductsPage.tsx
@@ -25,7 +25,7 @@ const ProductsPage = () => {
       id: 'mybestea',
       name: 'Mybestea',
       category: 'Minuman',
-      price: 'Rp 2.5 Juta',
+      price: 'Rp 2.9 Juta',
       image: 'https://raw.githubusercontent.com/jafabogaartha/assets/refs/heads/main/My%20Bestea%20(2).jpeg',
       heroImage: 'https://images.pexels.com/photos/1346155/pexels-photo-1346155.jpeg?auto=compress&cs=tinysrgb&w=1200',
       rating: 4.9,
